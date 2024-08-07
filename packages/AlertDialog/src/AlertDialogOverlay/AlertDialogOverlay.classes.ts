32bdf9a5d57e044661c76178c32319f7f8fd5c10
Andre <andre.peregrina@gmail.com>
2024-08-06 22:09:53 -0700

    [MTUI-46] Fix modals scrim opacity and fix divider in accordion

--- packages/AlertDialog/src/AlertDialogOverlay/AlertDialogOverlay.classes.ts
+++ packages/AlertDialog/src/AlertDialogOverlay/AlertDialogOverlay.classes.ts
@@ -1,2 +1,2 @@
 export const AlertDialogOverlayBaseClassName =
-  "alert-dialog-overlay-root bg-surface-scrim data-[state=open]:animate-overlayShow fixed inset-0 dark:bg-surface-scrim-dark";
+  "alert-dialog-overlay-root bg-surface-scrim/[0.32] data-[state=open]:animate-overlayShow fixed inset-0 dark:bg-surface-dark-scrim/[0.32]";
--- packages/Dialog/src/DialogOverlay/DialogOverlay.classes.ts
+++ packages/Dialog/src/DialogOverlay/DialogOverlay.classes.ts
@@ -1,1 +1,1 @@
-export const DialogOverlayBaseClassName = "dialog-overlay-root bg-surface-scrim data-[state=open]:animate-overlayShow fixed inset-0 dark:bg-surface-scrim-dark";
+export const DialogOverlayBaseClassName = "dialog-overlay-root bg-surface-scrim/[0.32] data-[state=open]:animate-overlayShow fixed inset-0 dark:bg-surface-dark-scrim/[0.32]";
