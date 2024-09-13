3c4fbe028f6839838091f0e5388b2a85cc8b0df2
Andre <andre.peregrina@gmail.com>
2024-09-12 18:40:14 -0700

    [MTUI-57] Add path only in production

--- apps/www/next.config.mjs
+++ apps/www/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     output: 'export',
+    basePath: isProd?'/material-tailwind-ui' : '',
+    assetPrefix: isProd?'/material-tailwind-ui' : '',
 };
 
 export default nextConfig;
