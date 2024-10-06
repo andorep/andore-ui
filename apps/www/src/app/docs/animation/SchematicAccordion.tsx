96a5a36807a5f93436ff204b300111617d6ab252
Andre <andre.peregrina@gmail.com>
2024-10-06 03:21:06 -0700

    [MTUI-82] Add duration and timing functions to animations docs

--- apps/www/src/app/docs/animation/SchematicAccordion.tsx
+++ apps/www/src/app/docs/animation/SchematicAccordion.tsx
@@ -1,20 +1,74 @@
 "use client";
 
-import React from "react";
+import React, {useRef} from "react";
 import {
   Accordion,
   AccordionContent,
   AccordionHeader,
   AccordionItem,
 } from "@andore-ui/accordion";
+import FloatingPointer from "@/app/_components/FloatingPointer/FloatingPointer";
+import {ListItem, ListItemText} from "@andore-ui/list";
+import {arrow, offset, useFloating, useMergeRefs} from "@floating-ui/react";
 
 const SchematicButton = () => {
+    const arrowBackgroundRef = useRef(null);
+    const arrowTextRef = useRef(null);
+    const arrowExtraRef = useRef(null);
+
+    const {
+        refs: refsExtra,
+        floatingStyles: floatingStylesExtra,
+        middlewareData: middlewareDataExtra,
+    } = useFloating({
+        placement: "left",
+        middleware: [
+            offset(20),
+            arrow({
+                element: arrowExtraRef,
+            }),
+        ],
+    });
+
+    const {
+        refs: refsBackground,
+        floatingStyles: floatingStylesBackground,
+        middlewareData: middlewareDataBackground,
+    } = useFloating({
+        placement: "left",
+        middleware: [
+            offset(20),
+            arrow({
+                element: arrowBackgroundRef,
+            }),
+        ],
+    });
+
+    const {
+        refs: refsText,
+        floatingStyles: floatingStylesText,
+        middlewareData: middlewareDataText,
+    } = useFloating({
+        placement: "right",
+        middleware: [
+            offset(15),
+            arrow({
+                element: arrowTextRef,
+            }),
+        ],
+    });
+
+
+    const ref = useMergeRefs([
+        refsBackground.setReference,
+        refsText.setReference,
+    ]);
 
   return (
     <div className={"w-[300px] h-fit p-4 bg-surface-container-lowest dark:bg-surface-dark-DEFAULT"}>
-        <Accordion type={"single"} collapsible defaultValue={'1'}>
+        <Accordion type={"single"} collapsible defaultValue={'1'} ref={ref}>
             <AccordionItem value={"1"}>
-                <AccordionHeader>Header 1</AccordionHeader>
+                <AccordionHeader ref={refsExtra.setReference}>Header 1</AccordionHeader>
                 <AccordionContent>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec
                 </AccordionContent>
@@ -36,6 +90,45 @@ const SchematicButton = () => {
                 </AccordionContent>
             </AccordionItem>
         </Accordion>
+        <FloatingPointer
+            placement={"left"}
+            ref={refsBackground.setFloating}
+            arrowRef={arrowBackgroundRef}
+            middlewareData={middlewareDataBackground}
+            style={floatingStylesBackground}
+        >
+            <ListItem className={`w-max rounded-full h-fit`}>
+                <ListItemText>
+                    <span className={"font-medium"}>accordion-open</span>
+                </ListItemText>
+            </ListItem>
+        </FloatingPointer>
+        <FloatingPointer
+            placement={"right"}
+            ref={refsText.setFloating}
+            arrowRef={arrowTextRef}
+            middlewareData={middlewareDataText}
+            style={floatingStylesText}
+        >
+            <ListItem className={`w-max rounded-full h-fit`}>
+                <ListItemText>
+                    <span className={"font-medium"}>duration-medium2</span>
+                </ListItemText>
+            </ListItem>
+        </FloatingPointer>
+        <FloatingPointer
+            placement={"left"}
+            ref={refsExtra.setFloating}
+            arrowRef={arrowExtraRef}
+            middlewareData={middlewareDataExtra}
+            style={floatingStylesExtra}
+        >
+            <ListItem className={`w-max rounded-full h-fit`}>
+                <ListItemText>
+                    <span className={"font-medium"}>ease-emphasizedAccelerate</span>
+                </ListItemText>
+            </ListItem>
+        </FloatingPointer>
     </div>
   );
 };
